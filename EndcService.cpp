be99924ddf2e69ac29622031b9f52310b82b28de
mingowu <51348832+mingowu@users.noreply.github.com>
2019-07-23 09:29:18 +0800

    Add files via upload

---
+++ EndcService.cpp
@@ -0,0 +1,155 @@
+#include <boost/msm/front/state_machine_def.hpp>
+#include <boost/msm/front/functor_row.hpp>
+#include <boost/msm/back/state_machine.hpp>
+#include <variant>
+#include <list>
+#include <iostream>
+
+struct SgnbAdditionRequest {};
+struct SgnbReconfigurationComplete {};
+struct SgnbReleaseRequest {};
+struct UeContextRelease {};
+struct SgnbModificationRequest {};
+
+using X2Message = std::variant<SgnbAdditionRequest,
+                               SgnbReconfigurationComplete,
+                               SgnbReleaseRequest,
+                               UeContextRelease,
+                               SgnbModificationRequest>;
+
+struct Idle : public boost::msm::front::state<>
+{
+    struct handleSgnbAdditionRequest
+    {
+        template <class Fsm, class SourceState, class TargetState>
+        void operator()(const SgnbAdditionRequest& p_event, Fsm& p_fsm, SourceState&, TargetState&)
+        {
+            p_fsm.processEvent(p_event);
+        }
+    };
+};
+
+struct WaitSgnbReconfComp : public boost::msm::front::state<>
+{
+    struct handleSgnbReconfComp
+    {
+        template <class Fsm, class SourceState, class TargetState>
+        void operator()(const SgnbReconfigurationComplete& p_event, Fsm& p_fsm, SourceState&, TargetState&)
+        {
+            p_fsm.processEvent(p_event);
+        }
+    };
+};
+
+struct ScgActive : public boost::msm::front::state<>
+{
+    struct handleSgnbReleaseRequest
+    {
+        template <class Fsm, class SourceState, class TargetState>
+        void operator()(const SgnbReleaseRequest& p_event, Fsm& p_fsm, SourceState&, TargetState&)
+        {
+            p_fsm.processEvent(p_event);
+        }
+    };
+
+    struct handleSgnbModificationRequest
+    {
+        template <class Fsm, class SourceState, class TargetState>
+        void operator()(const SgnbModificationRequest& p_event, Fsm& p_fsm, SourceState&, TargetState&)
+        {
+            p_fsm.processEvent(p_event);
+        }
+    };
+};
+
+struct WaitUeContextRelease : public boost::msm::front::state<>
+{
+    struct handleUeContextRelease
+    {
+        template <class Fsm, class SourceState, class TargetState>
+        void operator()(const UeContextRelease& p_event, Fsm& p_fsm, SourceState&, TargetState&)
+        {
+            p_fsm.processEvent(p_event);
+        }
+    };
+};
+
+struct isNotOk
+{
+    template <class Event, class Fsm, class SourceState, class TargetState>
+    bool operator()(const Event&, Fsm&, SourceState&, TargetState&)
+    {
+        std::cout << "unexpected message\n";
+        return true;
+    }
+};
+
+struct EndcService : public boost::msm::front::state_machine_def<EndcService>
+{
+    using initial_state = Idle;
+
+    struct transition_table : boost::mpl::vector<
+    //  Start                                        Event                        Target                Action                                             Guard
+        boost::msm::front::Row<Idle,                 SgnbAdditionRequest,         WaitSgnbReconfComp,   Idle::handleSgnbAdditionRequest,                   boost::msm::front::none>,
+        boost::msm::front::Row<WaitSgnbReconfComp,   SgnbReconfigurationComplete, ScgActive,            WaitSgnbReconfComp::handleSgnbReconfComp,          boost::msm::front::none>,
+        boost::msm::front::Row<ScgActive,            SgnbReleaseRequest,          WaitUeContextRelease, ScgActive::handleSgnbReleaseRequest,               boost::msm::front::none>,
+        boost::msm::front::Row<ScgActive,            SgnbModificationRequest,     WaitSgnbReconfComp,   ScgActive::handleSgnbModificationRequest,          boost::msm::front::none>,
+        boost::msm::front::Row<WaitUeContextRelease, UeContextRelease,            Idle,                 boost::msm::front::none,                           boost::msm::front::none>> {};
+
+   void processEvent(const SgnbAdditionRequest&)
+   {
+       std::cout << "process SgnbAdditionRequest\n";
+   }
+   void processEvent(const SgnbReconfigurationComplete&)
+   {
+       std::cout << "process SgnbReconfigurationComplete\n";
+   }
+   void processEvent(const SgnbReleaseRequest&)
+   {
+       std::cout << "process SgnbReleaseRequest\n";
+   }
+   void processEvent(const SgnbModificationRequest&)
+   {
+       std::cout << "process SgnbModificationRequest\n";
+   }
+   void processEvent(const UeContextRelease&)
+   {
+       std::cout << "process UeContextRelease\n";
+   }
+};
+
+class X2MessageVisitor
+{
+public:
+    X2MessageVisitor() { m_endcService.start(); }
+
+    template <typename T>
+    void operator()(const T& p_message)
+    {
+        m_endcService.process_event(p_message);
+    }
+private:
+    boost::msm::back::state_machine<EndcService> m_endcService{};
+};
+
+void testEndcService()
+{
+    std::list<X2Message> l_X2Messages = {
+        SgnbAdditionRequest{},
+        SgnbReconfigurationComplete{},
+        SgnbModificationRequest{},
+        SgnbReconfigurationComplete{},
+        SgnbReleaseRequest{},
+        UeContextRelease{}};
+    X2MessageVisitor l_X2MessageVisitor{};
+    for (const auto& l_X2Message : l_X2Messages)
+    {
+        std::visit(l_X2MessageVisitor, l_X2Message);
+    }
+}
+
+int main()
+{
+    testEndcService();
+    return 0;
+}
